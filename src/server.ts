264bc34facb1385550e826991ee34c60b1b076e1
Victor <129621220+hash-cell@users.noreply.github.com>
2026-01-02 19:11:06 -0300

    Refactor server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-// src/server.ts
 import express, { Request, Response } from 'express';
-import { router } from './routes/routes'; // <--- Aponta para o arquivo de rotas
+import { router } from './routes/routes'; 
 
 const app = express();
 app.use(express.json());
 
-app.use(router); // <--- Habilita as rotas
+app.use(router);
 
-// Mantenha a porta 3333 para não dar erro de novo
 const PORT = 3333; 
 
 app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`));
